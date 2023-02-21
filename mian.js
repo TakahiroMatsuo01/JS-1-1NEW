a5058d6156d34fba7fb0685755afd417a5e9046a
kenpi <miturugi.infinite@gmail.com>
2023-02-21 15:45:17 +0900

    修正後2

--- mian.js
+++ mian.js
@@ -2,14 +2,14 @@
 for(var i=1; i<=100; i++){
   if (i % 3 === 0 && i % 5 === 0){
   var FizzBuzz =(Fizz * Buzz)/(3*5*i);
-  console.log("Fizz,Buzz",FizzBuzz);
+  console.log('Fizz,Buzz',FizzBuzz);
   }
 else if (i % 3 === 0){
 var Fizz = 3 *i;
-  console.log("Fizz",Fizz/3);
+  console.log('Fizz',Fizz/3);
 }
 else if (i % 5 === 0){
 var Buzz = 5 *i;
-  conssole.log("Buzz",Buzz/5);
+  conssole.log('Buzz',Buzz/5);
 }
 }
