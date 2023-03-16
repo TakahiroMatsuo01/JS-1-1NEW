285c70a595d136bf0bb5f03edda2ddb1f1a07f10
codewriterX <miturugi.infinite@gmail.com>
2023-03-16 18:08:19 +0900

    2023-03-16-18:08

--- mian.js
+++ mian.js
@@ -1,15 +1,14 @@
 
-for(var i=1; i<=100; i++){
+for(let i=1; i<=100; i++){
   if (i % 3 === 0 && i % 5 === 0){
-  var FizzBuzz =(Fizz * Buzz)/(3*5*i);
-  console.log("Fizz,Buzz",FizzBuzz);
+  console.log('FizzBuzz',i);
   }
-else if (i % 3 === 0){
-var Fizz = 3 *i;
-  console.log("Fizz",Fizz/3);
-}
-else if (i % 5 === 0){
-var Buzz = 5 *i;
-  conssole.log("Buzz",Buzz/5);
-}
+
+  else if (i % 3 === 0){
+    console.log('Fizz',i);
+  }
+
+  else if (i % 5 === 0){
+    console.log('Buzz',i);
+  }
 }
