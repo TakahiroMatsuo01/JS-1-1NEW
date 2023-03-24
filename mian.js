dca5be1a592a6428fff0345479a293ac863ecaeb
codewriterX <miturugi.infinite@gmail.com>
2023-03-24 19:11:19 +0900

    修正後-2023-03-24-19:11

--- mian.js
+++ mian.js
@@ -1,14 +1,21 @@
+let Fizz = 3 *i;
+let Buzz = 5 *i;
+let FizzNumber = 3;
+let BuzzNumber = 5;
 
-for(let i=1; i<=100; i++){
+for(var i=1; i<=100; i++){
+  
   if (i % 3 === 0 && i % 5 === 0){
-  console.log('FizzBuzz',i);
+    let FizzBuzz =(Fizz * Buzz)/(FizzNumber*BuzzNumber*i);
+    console.log('FizzBuzz',FizzBuzz);
   }
 
   else if (i % 3 === 0){
-    console.log('Fizz',i);
+      console.log('Fizz',Fizz/FizzNumber);
   }
 
   else if (i % 5 === 0){
-    console.log('Buzz',i);
+      console.log('Buzz',Buzz/BuzzNumber);
   }
+
 }
