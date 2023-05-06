ed3dd0fde889ccb3adcf034cbc7ae02b04375410
codewriterX <miturugi.infinite@gmail.com>
2023-05-06 10:17:55 +0900

    修正後

--- mian.js
+++ mian.js
@@ -1,12 +1,12 @@
 
-  // let Fizz = 3 *i;
-  // let Buzz = 5 *i;
+  // const Fizz = 3 *i;
+  // const Buzz = 5 *i;
 
   //****for文の中にiがあるのに初期値として用意しないといけないと思ってました****/
-  // let FizzNumber = 3;
-  // let BuzzNumber = 5;
+  // const FizzNumber = 3;
+  // const BuzzNumber = 5;
 /********条件式でフィルタリングができていることに気づいていませんでした******/
-  for(var i=1; i<=100; i++){
+  for(let i=1; i<=100; i++){
     
     if (i % 3 === 0 && i % 5 === 0){
       // let FizzBuzz =(Fizz * Buzz)/(FizzNumber*BuzzNumber*i);
